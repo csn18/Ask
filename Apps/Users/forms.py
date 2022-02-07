84bf1ebad6532966f6e18f7bd76416677439253d
forest <formcsp@gmail.com>
2022-02-07 23:41:18 +0300

    Added button for open popup window with settings. Added avatar on nav-menu for view auth user

--- Apps/Users/forms.py
+++ Apps/Users/forms.py
@@ -4,6 +4,8 @@ from Apps.Users.models import CustomUser
 
 
 class CustomUserForm(forms.ModelForm):
+    age = forms.fields.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
+
     class Meta:
         model = CustomUser
-        fields = []
+        fields = ['username', 'first_name', 'last_name', 'age', 'gender']
