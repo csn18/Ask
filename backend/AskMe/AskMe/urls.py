3487dfc28680139c44129b50232c76464fd70617
forest <formcsp@gmail.com>
2022-02-13 22:05:08 +0300

    Added Djoser for registration and authorization user with API. Without verify email. Added module CORS (corsheaders) for accept requests from ReactJs

--- backend/AskMe/AskMe/urls.py
+++ backend/AskMe/AskMe/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('Apps.Users.urls'))
+    path('accounts/', include('Apps.Users.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
